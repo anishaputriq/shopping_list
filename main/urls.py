10be9492cdb9982d85941902e3097ba29d99ffca
Anisha Putri Qonitah <nishaputriq@gmail.com>
2023-11-15 14:47:28 +0700

    tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from main.views import show_main
+from main.views import create_product_flutter, show_main
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import register , get_product_json, add_product_ajax
 from main.views import login_user 
@@ -24,4 +24,5 @@ urlpatterns = [
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
     path('auth/', include('authentication.urls')),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
